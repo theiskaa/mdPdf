137c727e02b39f53119059d9d9c9f3c8874c30b8
Ismael Sh <me@theiskaa.com>
2024-08-28 00:54:39 +0400

    feat(assets): remove test_data and move testing markdowns on local only

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,7 +2,7 @@ use mpd;
 use std::{fs, path::Path};
 
 fn main() {
-    let file_path = Path::new("src/lib/test_data/nt.md");
+    let file_path = Path::new("assets/data/nt.md");
     let markdown = fs::read_to_string(file_path).expect("Failed to read test file");
     println!("{}", markdown);
     mpd::parse(markdown);
