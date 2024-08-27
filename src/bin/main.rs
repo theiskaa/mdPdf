fce4a524900e845bc1fd4d43b8dcd54ad649fcb6
Ismael Sh <me@theiskaa.com>
2024-08-28 00:45:17 +0400

    feat(lib): remove markdown lexer
    don't have that much time to improve it gonna use pulldown-cmark

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,5 +4,6 @@ use std::{fs, path::Path};
 fn main() {
     let file_path = Path::new("src/lib/test_data/nt.md");
     let markdown = fs::read_to_string(file_path).expect("Failed to read test file");
+    println!("{}", markdown);
     mpd::parse(markdown);
 }
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,19 +1,5 @@
-pub mod markdown_lexer;
-
-use markdown_lexer::*;
+#![allow(unused_variables)]
 
 pub fn parse(markdown: String) {
-    let mut lexer = Lexer::new(markdown);
-    match lexer.parse() {
-        Ok(tokens) => {
-            for token in tokens {
-                println!("{:?}", token);
-            }
-        }
-        Err(e) => {
-            eprintln!("Error while parsing: {:?}", e);
-        }
-    }
+    todo!("Implement parsing here");
 }
-
-
