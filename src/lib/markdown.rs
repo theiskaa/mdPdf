7d24395269831ad5c3e2a88d84511c165fb4adb9
Ismael Sh <me@theiskaa.com>
2024-10-01 01:00:47 +0400

    feat(markdown): make Token clonable

--- src/lib/markdown.rs
+++ src/lib/markdown.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     Heading(Vec<Token>, usize), // (content, level)
     Emphasis { level: usize, content: Vec<Token> },
