68048d9452b40b5f0e7ee4c03e83169bac894572
Ismael Sh <me@theiskaa.com>
2024-10-06 03:07:07 +0400

    feat(styling): implement styling on pdf, to create pdfs based on style match

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -4,11 +4,12 @@ pub mod styling;
 
 use markdown::*;
 use pdf::Pdf;
+use styling::StyleMatch;
 
 pub fn parse(markdown: String, path: &str) {
     let mut lexer = Lexer::new(markdown);
     let tokens = lexer.parse().expect("Failed to parse your markdown");
     let parser = Pdf::new(tokens);
-    let document = parser.create_document();
+    let document = parser.create_document(StyleMatch::default());
     Pdf::render(document, path);
 }
